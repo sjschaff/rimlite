03664a36e31cea9f31ead70b9b39b7c99ffb4a81
Simon Schaffer <SimonSchaffer1@gmail.com>
2019-09-26 01:39:54 -0700

    Optimized tilemap initialization

--- Assets/Script/MapTiler.cs
+++ Assets/Script/MapTiler.cs
@@ -51,7 +51,10 @@ public class BaseTile : TileBase
 
 public abstract class VirtualTileBase : Tile
 {
+    protected abstract bool HasSprite(BBTile tile, Vec2I pos, Vec2I subTile);
     protected abstract TileSprite GetSprite(BBTile tile, Vec2I pos, Vec2I subTile);
+
+    public static bool disableRefresh = false;
 
     protected MapTiler tiler { get; private set; }
     private Tilemap tilemap;
@@ -73,7 +76,7 @@ public abstract class VirtualTileBase : Tile
         Vec2I subTile = GridToSubTile(gridPos);
         BBTile tile = GetTile(pos);
 
-        TileSprite sprite = GetSprite(tile, pos, subTile);
+        TileSprite sprite = HasSprite(tile, pos, subTile) ? GetSprite(tile, pos, subTile) : null;
 
         tileData = new TileData
         {
@@ -90,17 +93,24 @@ public abstract class VirtualTileBase : Tile
     }
 
 
-    public override void RefreshTile(Vec3I pos, ITilemap tilemap)
+    public override void RefreshTile(Vec3I gridPos, ITilemap tilemap)
     {
-        var p = GridToTile(pos);
-        var r = GridToSubTile(pos);
+        Vec2I pos = GridToTile(gridPos);
+        Vec2I subTile = GridToSubTile(gridPos);
+        BBTile tile = GetTile(pos);
+
+        if (!disableRefresh || HasSprite(tile, pos, subTile))
+            tilemap.RefreshTile(gridPos);
+
+        if (disableRefresh)
+            return;
 
         for (int tx = 0; tx < 2; ++tx)
         {
             for (int ty = 0; ty < 2; ++ty)
             {
-                Vec2I t = p + new Vec2I(tx + r.x - 1, ty + r.y - 1);
-                if (t != p)
+                Vec2I t = pos + new Vec2I(tx + subTile.x - 1, ty + subTile.y - 1);
+                if (t != pos)
                 {
                     t *= 2;
                     for (int i = 0; i < 4; ++i)
@@ -108,19 +118,25 @@ public abstract class VirtualTileBase : Tile
                 }
             }
         }
-
-        tilemap.RefreshTile(pos);
     }
 }
 
 public class VirtualTileTerrainBase : VirtualTileBase
 {
+    protected override bool HasSprite(BBTile tile, Vec2I pos, Vec2I subTile) => true;
+
     protected override TileSprite GetSprite(BBTile tile, Vec2I pos, Vec2I subTile)
         => TerrainStandard.GetSprite(tiler, TerrainStandard.Terrain.Grass, TerrainStandard.TileType.Base, 0);
 }
 
 public class VirtualTileTerrainOver : VirtualTileBase
 {
+    protected override bool HasSprite(BBTile tile, Vec2I pos, Vec2I subTile)
+    {
+        var terrainStandard = tile.terrain as TerrainStandard;
+        return terrainStandard == null || terrainStandard.terrain != TerrainStandard.Terrain.Grass;
+    }
+
     protected override TileSprite GetSprite(BBTile tile, Vec2I pos, Vec2I subTile)
         => tile.terrain.GetSprite(tiler, pos, subTile);
 
@@ -141,24 +157,20 @@ public class VirtualTileTerrainOver : VirtualTileBase
 
 public class VirtualTileBuilding : VirtualTileBase
 {
-    protected override TileSprite GetSprite(BBTile tile, Vec2I pos, Vec2I subTile)
-    {
-        if (tile.hasBuilding && (tile.building.tiledRender || subTile == Vec2I.zero))
-            return tile.building.GetSprite(tiler, pos, subTile);
+    protected override bool HasSprite(BBTile tile, Vec2I pos, Vec2I subTile)
+        => tile.hasBuilding && (tile.building.tiledRender || subTile == Vec2I.zero);
 
-        return null;
-    }
+    protected override TileSprite GetSprite(BBTile tile, Vec2I pos, Vec2I subTile)
+        => tile.building.GetSprite(tiler, pos, subTile);
 }
 
 public class VirtualTileBuildingOver : VirtualTileBase
 {
-    protected override TileSprite GetSprite(BBTile tile, Vec2I pos, Vec2I subTile)
-    {
-        if (tile.hasBuilding && tile.building.oversized && subTile == Vec2I.zero)
-            return tile.building.GetSpriteOver(tiler, pos);
+    protected override bool HasSprite(BBTile tile, Vec2I pos, Vec2I subTile)
+        => tile.hasBuilding && tile.building.oversized && subTile == Vec2I.zero;
 
-        return null;
-    }
+    protected override TileSprite GetSprite(BBTile tile, Vec2I pos, Vec2I subTile)
+        => tile.building.GetSpriteOver(tiler, pos);
 }
 
 public class MapTiler
@@ -174,6 +186,13 @@ public class MapTiler
             this.tilemap = tilemap;
             vtileA = VirtualTileBase.Create<T>(tilemap, tiler);
             vtileB = VirtualTileBase.Create<T>(tilemap, tiler);
+        }
+
+        public void InitTilemap(BoundsInt bounds, TileBase[] buffer)
+        {
+            for (int i = 0; i < buffer.Length; ++i)
+                buffer[i] = vtileA;
+            tilemap.SetTilesBlock(bounds, buffer);
         }
 
         public void UpdateTile(Vec2I v)
@@ -192,37 +211,40 @@ public class MapTiler
     public Atlas sprites32 { get; private set; }
     public Atlas sprites64 { get; private set; }
 
+    private readonly TilemapUpdater<VirtualTileTerrainBase> tilemapTerrain;
     private readonly TilemapUpdater<VirtualTileTerrainOver> tilemapTerrainOver;
     private readonly TilemapUpdater<VirtualTileBuilding> tilemapBuilding;
     private readonly TilemapUpdater<VirtualTileBuildingOver> tilemapBuildingOver;
 
     public MapTiler(Map map)
     {
+        System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+
         this.map = map;
         tileset32 = new Atlas(map.tileset32, 16, 32);
         tileset64 = new Atlas(map.tileset64, 32, 64);
         sprites32 = new Atlas(map.sprites32, 8, 32);
         sprites64 = new Atlas(map.sprites64, 16, 64);
 
+        tilemapTerrain = new TilemapUpdater<VirtualTileTerrainBase>(this, map.terrainBase);
         tilemapTerrainOver = new TilemapUpdater<VirtualTileTerrainOver>(this, map.terrainOver);
         tilemapBuilding = new TilemapUpdater<VirtualTileBuilding>(this, map.buildingBase);
         tilemapBuildingOver = new TilemapUpdater<VirtualTileBuildingOver>(this, map.buildingOver);
 
-        var vtileBase = VirtualTileBase.Create<VirtualTileTerrainBase>(map.terrainBase, this);
-        for (int x = 0; x < map.size.x * 2; ++x)
-            for (int y = 0; y < map.size.y * 2; ++y)
-                map.terrainBase.SetTile(new Vec3I(x, y, 0), vtileBase);
+        sw.Start();
+        var bounds = new BoundsInt(0, 0, 0, map.size.x * 2, map.size.y * 2, 1);
+        var tileBuffer = new TileBase[bounds.size.x * bounds.size.y];
 
-        for (int x = 0; x < map.size.x; ++x)
-        {
-            for (int y = 0; y < map.size.y; ++y)
-            {
-                var gridPos = new Vec2I(x, y);
-                tilemapTerrainOver.UpdateTile(gridPos);
-                tilemapBuilding.UpdateTile(gridPos);
-                tilemapBuildingOver.UpdateTile(gridPos);
-            }
-        }
+        VirtualTileBase.disableRefresh = true;
+        tilemapTerrain.InitTilemap(bounds, tileBuffer);
+        tilemapTerrainOver.InitTilemap(bounds, tileBuffer);
+        tilemapBuilding.InitTilemap(bounds, tileBuffer);
+        tilemapBuildingOver.InitTilemap(bounds, tileBuffer);
+        VirtualTileBase.disableRefresh = false;
+
+        sw.Stop();
+        Debug.Log("tiles took " + sw.ElapsedMilliseconds + "ms");
+        sw.Reset();
     }
 
     public void UpdateTerrain(Vec2I tile) => tilemapTerrainOver.UpdateTile(tile);
