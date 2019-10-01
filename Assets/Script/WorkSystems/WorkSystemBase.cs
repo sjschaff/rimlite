3cb130da006f8bf1c1dcaf9fde9ca131b8f9fe3b
Simon Schaffer <SimonSchaffer1@gmail.com>
2019-09-30 22:08:02 -0700

    finished split worksystem

--- Assets/Script/WorkSystems/WorkSystemBase.cs
+++ Assets/Script/WorkSystems/WorkSystemBase.cs
@@ -7,61 +7,6 @@ using Vec2I = UnityEngine.Vector2Int;
 
 namespace BB
 {
-    public class JobHandle
-    {
-        public readonly IWorkSystem system;
-        public readonly Vec2I pos;
-        public Work activeWork;
-
-        public JobHandle(IWorkSystem system, Vec2I pos)
-        {
-            BB.Assert(system != null);
-            this.system = system;
-            this.pos = pos;
-        }
-
-        public virtual void CancelJob() => system.CancelJob(this);
-        public virtual void AbandonWork() => system.WorkAbandoned(this);
-
-        public virtual void Destroy()
-        {
-            if (activeWork != null)
-                activeWork.Cancel();
-        }
-    }
-
-    // example: s
-    public interface IWorkSystem
-    {
-        // TODO: some way to track buildings added/removed
-        IOrdersGiver orders { get; }
-
-        IEnumerable<Work> QueryWork();
-
-        void CancelJob(JobHandle job);
-        void WorkAbandoned(JobHandle job);
-    }
-
-    [Flags]
-    public enum OrdersFlags
-    {
-        None = 0,
-        AppliesItem = 1,
-        AppliesBuilding = 2,
-        AppliesGlobally = 4,
-    }
-
-    public interface IOrdersGiver
-    {
-        bool HasOrder(Vec2I pos);
-        void AddOrder(Vec2I pos);
-        Transform CreateOverlay(Vec2I pos);
-
-        OrdersFlags flags { get; }
-        bool ApplicableToItem(Item item);
-        bool ApplicableToBuilding(IBuilding building);
-    }
-
     public abstract class WorkSystemStandard<TThis, TJob> : IWorkSystem
         where TJob : WorkSystemStandard<TThis, TJob>.JobStandard
         where TThis : WorkSystemStandard<TThis, TJob>
