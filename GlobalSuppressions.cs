d502ab4d97f8fe99ca30f8865445222255b273d6
Simon Schaffer <SimonSchaffer1@gmail.com>
2019-10-07 02:46:51 -0700

    Added the rest of priority queue

---
+++ GlobalSuppressions.cs
@@ -0,0 +1,13 @@
+﻿
+// This file is used by Code Analysis to maintain SuppressMessage 
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given 
+// a specific target and scoped to a namespace, type, member, etc.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "External Source", Scope = "member", Target = "~P:Priority_Queue.SimplePriorityQueue`2.SimpleNode.Data")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0034:Simplify 'default' expression", Justification = "External Source", Scope = "member", Target = "~M:Priority_Queue.SimplePriorityQueue`2.TryFirst(`0@)~System.Boolean")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0034:Simplify 'default' expression", Justification = "External Source", Scope = "member", Target = "~M:Priority_Queue.SimplePriorityQueue`2.TryDequeue(`0@)~System.Boolean")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0034:Simplify 'default' expression", Justification = "External Source", Scope = "member", Target = "~M:Priority_Queue.SimplePriorityQueue`2.TryGetPriority(`0,`1@)~System.Boolean")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0018:Inline variable declaration", Justification = "External Source", Scope = "member", Target = "~M:Priority_Queue.SimplePriorityQueue`2.GetExistingNode(`0)~Priority_Queue.SimplePriorityQueue`2.SimpleNode`0")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0018:Inline variable declaration", Justification = "External Source", Scope = "member", Target = "~M:Priority_Queue.SimplePriorityQueue`2.AddToNodeCache(Priority_Queue.SimplePriorityQueue`2.SimpleNode`0)")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0018:Inline variable declaration", Justification = "External Source", Scope = "member", Target = "~M:Priority_Queue.SimplePriorityQueue`2.RemoveFromNodeCache(Priority_Queue.SimplePriorityQueue`2.SimpleNode`0)")]
